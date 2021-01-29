aa35d81f01b56010b4f36f004d4f3260dbe9eb12
karthi9788 <tkkarthi9788@gmail.com>
2021-01-29 13:02:50 +0530

    Update CartDaoCollectionImpl.java

--- src/com/cognizant/truyum/dao/CartDaoCollectionImpl.java
+++ src/com/cognizant/truyum/dao/CartDaoCollectionImpl.java
@@ -1,5 +1,77 @@
 package com.cognizant.truyum.dao;
 
-public class CartDaoCollectionImpl implements CartDao{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+
+import com.cognizant.truyum.model.Cart;
+import com.cognizant.truyum.model.MenuItem;
+
+public class CartDaoCollectionImpl implements CartDao {
+
+	private static HashMap<Long, Cart> userCarts;
+
+	@Override
+	// add to cart
+	public void addCartItem(long userId, long menuItemId) {
+		// TODO Auto-generated method stub
+		List<MenuItem> menuItemList;
+		MenuItemDao menuItemDao = new MenuItemDaoCollectionImpl();
+		MenuItem menuItem = menuItemDao.getMenuItem(menuItemId);
+
+		if (userCarts.containsKey(userId)) {
+			Cart cart = userCarts.get(userId);
+			menuItemList = cart.getMenuItemList();
+			menuItemList.add(menuItem);
+			cart.setMenuItemList(menuItemList);
+			cart.setTotal(cart.getTotal() + menuItem.getPrice());
+			userCarts.put(userId, cart);
+		} else {
+			menuItemList = new ArrayList<MenuItem>();
+			menuItemList.add(menuItem);
+			Cart cart = new Cart(menuItemList, menuItem.getPrice());
+			userCarts.put(userId, cart);
+
+		}
+	}
+
+	public CartDaoCollectionImpl() {
+		super();
+		// TODO Auto-generated constructor stub
+
+		if (userCarts == null) {
+			userCarts = new HashMap<Long, Cart>();
+		}
+	}
+
+	public List<MenuItem> getAllCartItems(long userId) throws CartEmptyException {
+
+		Cart cart = userCarts.get(userId);
+		List<MenuItem> menuItemList = cart.getMenuItemList();
+		if (menuItemList == null || menuItemList.size() == 0) {
+			throw new CartEmptyException("Cart is empty");
+		}
+		double total = 0.0;
+		for (MenuItem menu : menuItemList) {
+			total = total + menu.getPrice();
+		}
+		cart.setTotal(total);
+
+		return menuItemList;
+
+	}
+
+	public void removeCartItem(long userId, long menuItemId) {
+		Cart cart = userCarts.get(userId);
+		ListIterator<MenuItem> iterator = cart.getMenuItemList().listIterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().getId() == menuItemId) {
+				iterator.remove();
+			}
+		}
+
+	}
 
 }
+
