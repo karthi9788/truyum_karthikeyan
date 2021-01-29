0be12b4ddf8d974047993426d25032d3f217833c
karthi9788 <tkkarthi9788@gmail.com>
2021-01-29 13:01:52 +0530

    Update CartDao.java

--- src/com/cognizant/truyum/dao/CartDao.java
+++ src/com/cognizant/truyum/dao/CartDao.java
@@ -6,7 +6,9 @@ import com.cognizant.truyum.model.MenuItem;
 
 public interface CartDao {
 	public void addCartItem(long userId, long menuItemId);
+
 	public List<MenuItem> getAllCartItems(long userId) throws CartEmptyException;
-	public void removeCartItem(long userId,long menuItemId);
-	
-	}
+
+	public void removeCartItem(long userId, long menuItemId);
+
+}
