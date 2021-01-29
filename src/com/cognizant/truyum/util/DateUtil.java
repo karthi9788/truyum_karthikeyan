90a33079c16c903d2cef4b1b3226aacc39d49ee3
karthi9788 <tkkarthi9788@gmail.com>
2021-01-29 13:10:56 +0530

    Update DateUtil.java

--- src/com/cognizant/truyum/util/DateUtil.java
+++ src/com/cognizant/truyum/util/DateUtil.java
@@ -1,24 +1,22 @@
 package com.cognizant.truyum.util;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class DateUtil {
-	public  static Date convertToDate(String date) {
-		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+	public static Date convertToDate(String date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setLenient(false);
 		Date d = null;
 		try {
-			d= sdf.parse(date);
+			d = sdf.parse(date);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return d;
-		
+
 	}
-	
-	
 
 }
