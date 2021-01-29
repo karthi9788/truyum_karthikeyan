e7ee97f46ba87e8057158c1d0577275b2d50a17a
karthi9788 <tkkarthi9788@gmail.com>
2021-01-29 13:03:58 +0530

    Update CartEmptyException.java

--- src/com/cognizant/truyum/dao/CartEmptyException.java
+++ src/com/cognizant/truyum/dao/CartEmptyException.java
@@ -2,10 +2,12 @@ package com.cognizant.truyum.dao;
 
 public class CartEmptyException extends Exception {
 
-	/**
-	 * 
-	 */
-	
-	
+	private static final long serialVersionUID = 5455375227193462427L;
+
+	public CartEmptyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
 
 }
+
