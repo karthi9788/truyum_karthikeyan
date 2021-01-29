3b645cd623bee6ce38c3ebf0ed14acf7e9f49282
karthi9788 <tkkarthi9788@gmail.com>
2021-01-29 13:06:34 +0530

    Update MenuItemDaoCollectionImplTest.java

--- src/com/cognizant/truyum/dao/MenuItemDaoCollectionImplTest.java
+++ src/com/cognizant/truyum/dao/MenuItemDaoCollectionImplTest.java
@@ -2,22 +2,43 @@ package com.cognizant.truyum.dao;
 
 import java.util.List;
 import com.cognizant.truyum.model.MenuItem;
+import com.cognizant.truyum.util.DateUtil;
 
 public class MenuItemDaoCollectionImplTest {
-	
+
 	public static void main(String[] args) {
 		testGetMenuItemListAdmin();
-	
+		testGetMenuItemListCustomer();
+		testModifyMenuItem();
 
 	}
-	static void testGetMenuItemListAdmin() {
+
+	public static void testGetMenuItemListAdmin() {
 		// TODO Auto-generated method stub
-		MenuItemDao menuItemDao= new MenuItemDaoCollectionImpl();
-		List <MenuItem> menuItemList =  menuItemDao.getMenuItemListAdmin();
-		for(MenuItem val : menuItemList) {
+		MenuItemDao menuItemDao = new MenuItemDaoCollectionImpl();
+		List<MenuItem> menuItemList = menuItemDao.getMenuItemListAdmin();
+		for (MenuItem val : menuItemList) {
 			System.out.println(val.toString());
 		}
 	}
-	
+
+	public static void testGetMenuItemListCustomer() {
+		MenuItemDao menuItemDao = new MenuItemDaoCollectionImpl();
+		List<MenuItem> menuItemList = menuItemDao.getMenuItemListCustomer();
+		for (MenuItem val : menuItemList) {
+			System.out.println(val.toString());
+		}
+	}
+
+	public static void testModifyMenuItem() {
+		// TODO Auto-generated method stub
+
+		MenuItem menuItem = new MenuItem(2, "Cake", 80.00f, true, DateUtil.convertToDate("11/12/2019"), "Main Course",
+				false);
+		MenuItemDao menuItemDao = new MenuItemDaoCollectionImpl();
+		menuItemDao.modifyMenuItem(menuItem);
+		System.out.println(menuItemDao.getMenuItem(2));
+
+	}
 
 }
